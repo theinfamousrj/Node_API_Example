95fe3f3182a409b4aa0a686d640e72a9726d2a48
rj <raymondjohnhill@gmail.com>
2017-12-30 20:29:46 -0500

    Added static HTML page in /info (index.html). Added an image in /images (avatar.png). Modified server.js to reflect the previously mentioned static files. Added some vars for host and port. Modified the json body limit. Added /status and /version routes.

--- server.js
+++ server.js
@@ -6,6 +6,9 @@
 'use strict';
 console.time('boot time');
 
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || 'http://127.0.0.1';
+
 const app = require('express')();
 const express = require('express');
 const dateFormat = require('dateformat');
@@ -13,39 +16,58 @@ const bodyParser = require('body-parser');
 const compression = require('compression');
 
 
+//Ensure that we catch everything, just in case.
+process.on('unhandledRejection', (error) => {
+    console.error(error);
+});
+
+
 //Support for large encoded bodies
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 //Support for large json bodies
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.json({ limit: '10mb' }));
 
 //Begin using compression (standard gzip)
 app.use(compression());
 
 
-
 //BEGIN STATIC ROUTES
-
 app.use('/info', express.static('static/info'));
 
+app.use('/images', express.static('static/images'));
+
+app.use('/documentation', express.static('static/documentation'));
 //END STATIC ROUTES
 
 
+//BEGIN NON-STATIC ROUTES
+app.use('/status', (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
 
-app.use((req, res, next) => {
-    if(req.url === '/favicon.ico') {
-        //Return favicon stuff here, if it's requested.
-        res.writeHead(200, {'Content-Type': 'image/x-icon'} );
-        return res.end();
-    }
-
-    //if we have any initial setup it can go here.
+    return res.status(200).send({
+        status: 'LIVE'
+    });
 });
 
-app.listen(8080, () => {
+app.use('/version', (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    let fs = require('fs');
+    let obj = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+
+    return res.status(200).send({
+        version: obj.version || '1.0.0'
+    });
+});
+//END NON-STATIC ROUTES
+
+
+
+app.listen(port, () => {
     console.timeEnd('boot time');
 
     let now = new Date();
     dateFormat.masks.serverTime = "mm/dd/yy hh:MM:ss TT";
-    console.log('(%s) Listening on %s:%s', dateFormat(now, "serverTime"), (process.env.HOST || 'localhost'), (process.env.PORT || '8080'));
+    console.log('(%s) Listening on %s:%s', dateFormat(now, "serverTime"), host, port);
 });
